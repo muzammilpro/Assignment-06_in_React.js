b4da61919e245c9195591e3f91cb34ba9b876174
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-17 11:13:28 -0700

    Do Some Changes

--- src/Components/PopularItems/PopularItems.js
+++ src/Components/PopularItems/PopularItems.js
@@ -1,4 +1,9 @@
 import PopularItemsCards from '../PopularItemsCard/PopularItemsCards'
+import img1 from '../../Images/order 1.png'
+import img2 from '../../Images/order 2.png'
+import img3 from '../../Images/order3.png'
+import img4 from '../../Images/order 4.png'
+import img5 from '../../Images/order 5.png'
 
 export default function PopularItems() {
     return (
@@ -16,16 +21,16 @@ export default function PopularItems() {
                         </div>
 
 
-                        
-                            <PopularItemsCards />
-                            <PopularItemsCards />
-                            <PopularItemsCards />
-                            <PopularItemsCards />
-                            <PopularItemsCards />
 
-                        
+                        <PopularItemsCards scr={img1} title="Chees Burger" price="38.88" Text=" Burger Arina" />
+                        <PopularItemsCards scr={img2} title="Toffe's Cake" price="4.00" Text=" Top Sticks" />
+                        <PopularItemsCards scr={img3} title="Dancake" price="1.99" Text=" Cake World" />
+                        <PopularItemsCards scr={img4} title="Crispy Sandwitch" price="3.00" Text=" Fastfood Dine" />
+                        <PopularItemsCards scr={img5} title="Thai Soup" price="2.79" Text=" Foody man" />
 
-                       
+
+
+
 
 
                         <div className="col-1  d-flex align-items-center">
--- src/Components/PopularItemsCard/PopularItemsCards.js
+++ src/Components/PopularItemsCard/PopularItemsCards.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-export default function PopularItemsCards() {
+export default function PopularItemsCards(props) {
   return (
     <div className="col-lg-2 col-md-4 col-sm-12 mt-5">
     <div className="card border border-0 text-start mx-auto" style={{width: "12rem"}}>
-        <img src="./assets/order 1.png" className="card-img-end img-fluid mt-4 rounded-4" alt="image" />
+        <img src={props.scr} className="card-img-end img-fluid mt-4 rounded-4" alt="image" />
         <div className="card-body">
-            <h6 className="fw-bold"> Chees Burger</h6>
-            <h6 className="color-text"><i className="bi bi-geo-alt-fill"></i> Burger Arina </h6>
-            <h6>$38.88</h6>
+            <h6 className="fw-bold"> {props.title}r</h6>
+            <h6 className="color-text"><i className="bi bi-geo-alt-fill"></i> {props.Text} </h6>
+            <h6>${props.price}</h6>
             <button className="btn btn-lg  find-food btn-order text-white">Order Now</button>
         </div>
     </div>
